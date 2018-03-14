e80acd521ccb38050ef3a3f31ca3a8ecf6f01430
nnk <nnk@nnk-OptiPlex-3020.(none)>
2018-03-14 15:17:28 +0530

    integrate audio functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
+ 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-
+import { StreamingMedia } from '@ionic-native/streaming-media';
+ 
 @NgModule({
   declarations: [
     MyApp,
@@ -24,7 +25,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    StreamingMedia
   ]
 })
 export class AppModule {}
